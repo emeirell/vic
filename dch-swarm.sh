6d2ca594dea76d2261f9c67e6a17a07942d1bff5
Eduardo Meirelles <emeirell@hotmail.com>
2017-10-11 15:11:32 -0300

    added some variables for volumes and worker names

--- dch-swarm.sh
+++ dch-swarm.sh
@@ -7,45 +7,54 @@ NUM_WORKERS=3
 # this needs to be defined on your VCH using the '--container-network' option
 # use 'docker network ls' to list available external networks
 CONTAINER_NET=routable
+
+## Advanced Variables - YOU PROBABLY DONT NEED TO MODIFY BEYOND THIS POINT
 # Docker Container Host (DCH) image to use 
 # see https://hub.docker.com/r/vmware/dch-photon/tags/ for list of available Docker Engine versions
 DCH_IMAGE="vmware/dch-photon:17.06"
+# name of swarm master node
+NAME_MASTER="manager1"
+# name of the docker volume for the swarm master
+VOL_MASTER="registrycache"
+# Worker node prefix
+WORKER_PREFIX="worker"
 
+#########################################################
 ## NO NEED TO MODIFY BEYOND THIS POINT
 # pull the image
 docker pull $DCH_IMAGE
 
 # create a docker volume for the master image cache
-docker volume create --opt Capacity=10GB --name registrycache
+docker volume create --opt Capacity=10GB --name $VOL_MASTER
 # create and run the master instance
-docker run -d -v registrycache:/var/lib/docker \
+docker run -d -v $VOL_MASTER:/var/lib/docker \
   --net $CONTAINER_NET \
-  --name manager1 --hostname=manager1 \
+  --name $NAME_MASTER --hostname=$NAME_MASTER \
   $DCH_IMAGE
 # get the master IP
-SWARM_MASTER=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' manager1)
+SWARM_MASTER=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $NAME_MASTER)
 # create the new swarm on the master
-docker -H $SWARM_MASTER swarm init
+docker -H $SWARM_MASTER:2375 swarm init
 
 # get the join token
-SWARM_TOKEN=$(docker -H $SWARM_MASTER swarm join-token -q worker)
+SWARM_TOKEN=$(docker -H $SWARM_MASTER:2375 swarm join-token -q worker)
 sleep 10
 
 # run $NUM_WORKERS workers and use $SWARM_TOKEN to join the swarm
 for i in $(seq "${NUM_WORKERS}"); do
 
   # create docker volumes for each worker to be used as image cache
-  docker volume create  --opt Capacity=10GB --name worker-vol${i}
+  docker volume create  --opt Capacity=10GB --name $WORKER_PREFIX${i}-vol
   # run new worker container
-  docker run -d -v worker-vol${i}:/var/lib/docker \
+  docker run -d -v $WORKER_PREFIX${i}-vol:/var/lib/docker \
     --net $CONTAINER_NET \
-    --name worker${i} --hostname=worker${i}  \
+    --name $WORKER_PREFIX${i} --hostname=$WORKER_PREFIX${i}  \
     $DCH_IMAGE  
   # wait for daemon to start
   sleep 10
 
   # join worker to the swarm
-  for w in $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' worker${i}); do
+  for w in $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $WORKER_PREFIX${i}); do
     docker -H $w:2375 swarm join --token ${SWARM_TOKEN} ${SWARM_MASTER}:2377
   done
  
